5bab75f76d874b793538a5c7e6607be2a4857fae
Szymon Zwoliński <szymzwoo@gmail.com>
2022-06-07 23:17:46 +0200

    lokacja dodanie opisu oraz listy wyboru

--- resources/views/final_order_location/create.blade.php
+++ resources/views/final_order_location/create.blade.php
@@ -6,14 +6,21 @@
       
       <form action="{{ url('final_order_location/') }}" method="post">
         {!! csrf_field() !!}
-        <label>id_zamowienia</label></br>
-        <input type="text" name="id_zamowienia" id="id_zamowienia" class="form-control"></br>
-        <label>nadawca</label></br>
-        <input type="text" name="nadawca" id="nadawca" class="form-control"></br>
+        <label>Id zamówienia -> nadawca</label>
+        <select name="cars" id="cars">
+          @foreach($data as $item)
+          <option value="{{$item->nadawca}}">{{$item->ID_ZAMOWIENIA}} -> {{$item->nadawca}}</option>
+          @endforeach
+        </select>
+        <br>
+        <br>
         <label>kraj</label></br>
         <input type="text" name="kraj" id="kraj" class="form-control"></br>
         <label>miasto</label></br>
         <input type="text" name="miasto" id="miasto" class="form-control"></br>
+        <label>opis</label></br>
+        <input type="text" name="opis" id="opis" class="form-control"></br>
+
 
         <input type="submit" value="Dodaj" class="btn btn-success"></br>
     </form>
